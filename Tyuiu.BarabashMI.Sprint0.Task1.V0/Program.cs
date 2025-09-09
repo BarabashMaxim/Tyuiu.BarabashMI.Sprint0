aad8b1b5066dce4230ab3a43402521feda8bfb31
BarabashMaxim <Tyuiu.BarabashMI@outlook.com>
2025-09-09 21:31:27 +0500

    Task1

---
+++ Tyuiu.BarabashMI.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,11 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        string name = "Максим";
+        int age = 17;
+        Console.WriteLine("Здрвствуйте, меня зовут " + name + " мне " + age + " лет!");
+        Console.ReadKey();
+    }
+}
