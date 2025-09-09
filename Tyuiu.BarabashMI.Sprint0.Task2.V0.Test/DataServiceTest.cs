fa08155f8fc5d4aeba4f31e4ff6c7a48d17ed732
BarabashMaxim <Tyuiu.BarabashMI@outlook.com>
2025-09-09 21:45:54 +0500

    Task2

---
+++ Tyuiu.BarabashMI.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.BarabashMI.Sprint0.Task2.V0.Lib;
+namespace Tyuiu.BarabashMI.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetValid()
+        {
+            var name = "Максим";
+            var res = DataService.GetMessage(name);
+            Assert.AreEqual("Привет, Максим", res);
+        }
+    }
+}
---
+++ Tyuiu.BarabashMI.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,14 @@
+﻿using Tyuiu.BarabashMI.Sprint0.Task2.V0.Lib;
+namespace Tyuiu.BarabashMI.Sprint0.Task2.V0
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.GetMessage("Максим"));
+            Console.ReadKey();
+
+        }
+    }
+}
