db0ee658098eaed000b881e5d5e862efb0707c91
BarabashMaxim <Tyuiu.BarabashMI@outlook.com>
2025-09-13 19:25:58 +0500

    Task6

---
+++ Tyuiu.BarabashMI.Sprint0.Task6.V0/Program.cs
@@ -0,0 +1,12 @@
+﻿using Tyuiu.BarabashMI.Sprint0.Task6.V0.Lib;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] arraynums = { 1, 2, 3, 4, 5 };
+        Console.WriteLine("Сумма элементов массива = " + DataService.AdditionArray(arraynums));
+        Console.WriteLine("Разность элементов массива = " + DataService.SubtractionArray(arraynums));
+        Console.WriteLine("Произведение элементов массива = " + DataService.MultiplicatArray(arraynums));
+
+    }
+}
